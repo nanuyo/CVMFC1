e2e6ecc815a3e8f27408609b739d9f504d67bf7a
Hyung Jun Park <hjparkg71@gmail.com>
2020-09-14 21:35:32 +0900

    초기 커밋.

---
+++ CVMFC1.cpp
@@ -0,0 +1,52 @@
+﻿
+// CVMFC1.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "CVMFC1.h"
+#include "CVMFC1Dlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CCVMFC1App
+
+BEGIN_MESSAGE_MAP(CCVMFC1App, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CCVMFC1App 생성
+
+CCVMFC1App::CCVMFC1App()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CCVMFC1App 개체입니다.
+
+CCVMFC1App theApp;
+
+
+// CCVMFC1App 초기화
+
+BOOL CCVMFC1App::InitInstance()
+{
+	CWinApp::InitInstance();
+	
+
+	CCVMFC1Dlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+	
+
+	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고  응용 프로그램을 끝낼 수 있도록 FALSE를
+	// 반환합니다.
+	return FALSE;
+}
+
---
+++ CVMFC1.h
@@ -0,0 +1,32 @@
+﻿
+// CVMFC1.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CCVMFC1App:
+// 이 클래스의 구현에 대해서는 CVMFC1.cpp을(를) 참조하세요.
+//
+
+class CCVMFC1App : public CWinApp
+{
+public:
+	CCVMFC1App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCVMFC1App theApp;
